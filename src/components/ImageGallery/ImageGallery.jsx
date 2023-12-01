e583c9402a455f41b12bf7a0a7b31014ec6a888b
MoniekNK <monika.nowakowska01@gmail.com>
2023-12-01 23:06:49 +0100

    new

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import ImageGalleryItem from './ImageGalleryItem.module.css';
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import css from './ImageGallery.module.css';
 
 const ImageGallery = ({ images, onImageClick }) => {
   const lastNewImageRef = useRef();
